f156624ba47cab61335e02fad6700f43ce2d6429
Ollie <oliverdanielsmith@hotmail.com>
2021-02-16 13:05:03 +0000

    Can now provide alternative thank you message if the postcode is outside of the postcode range

--- client/src/components/ThankYou.js
+++ client/src/components/ThankYou.js
@@ -3,13 +3,29 @@ import '../assets/stylesheets/ThankYou.css';
 
 const ThankYou = props => {
 
+  const postcodeInRange = postcode => {
+    var postcodes = ["SW2", "SW4", "SW9", "SW8", "SW11", "SW12", "SW15", "SW18"]
+    for (let i = 0; i < postcodes.length; i++) {
+      var n = postcode.toUpperCase().search(`${postcodes[i]}`);
+      if (n === -1) {
+        continue;
+      } else {
+        return true;
+      }
+    }
+  };
+
   return (
     <div>
       <div id="thank-you-1">
         Thank you for registering your interest, {props.lastInterest.first_name}.
       </div><br />
       <div id="thank-you-2">
-        We already plan to deliver to this postcode. We will be in contact when we launch Udderless.
+        {postcodeInRange(props.lastInterest.postcode) === true ? (
+          "We already plan to deliver to this postcode. We will be in contact when we launch Udderless." 
+        ) : ( 
+          "We do not currently plan to deliver to this postcode. If more interest for this postcode is shown then it will be added to our range."
+        )}
       </div>
     </div>
   )
--- client/src/test/ThankYou.test.js
+++ client/src/test/ThankYou.test.js
@@ -1,13 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import ThankYou from '../components/ThankYou';
 
-var interestOne = {
-  email: "ollie@ollie.com",
-  first_name: "Ollie",
-  postcode: "SW9 5JJ",
-  tel: "0123455677"
-}
-
 test('renders positive thank you message for a given name and postcode', () => {
   render(<ThankYou lastInterest={interestOne}/>);
   const firstMessage = screen.getByText('Thank you for registering your interest, Ollie.');
@@ -15,3 +8,25 @@ test('renders positive thank you message for a given name and postcode', () => {
   expect(firstMessage).toBeInTheDocument();
   expect(secondMessage).toBeInTheDocument();
 });
+
+test('renders negative thank you message for a given name and postcode that is outside of delivery range', () => {
+  render(<ThankYou lastInterest={interestTwo}/>);
+  const firstMessage = screen.getByText('Thank you for registering your interest, Ollie.');
+  const secondMessage = screen.getByText('We do not currently plan to deliver to this postcode. If more interest for this postcode is shown then it will be added to our range.');
+  expect(firstMessage).toBeInTheDocument();
+  expect(secondMessage).toBeInTheDocument();
+});
+
+const interestOne = {
+  email: "ollie@ollie.com",
+  first_name: "Ollie",
+  postcode: "SW9 5JJ",
+  tel: "0123455677"
+}
+
+const interestTwo = {
+  email: "ollie@ollie.com",
+  first_name: "Ollie",
+  postcode: "RH5 5JJ",
+  tel: "0123455677"
+}
